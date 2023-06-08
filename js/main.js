5b7aafc242cf6aa9b3dfa406d67a22a9f2de9409
xytorenok <xytorenok@gmail.com>
2023-06-08 18:23:16 +0300

    7

--- js/main.js
+++ js/main.js
@@ -1,7 +1,43 @@
 const humburger = document.querySelector('.humburger');
 const curtain = document.querySelector('.curtain');
+const line = document.querySelector('.sect-1-clients-line-visible');
+const sticker = document.querySelector('.square-sticker-1-tablet');
+const curtainLinks = document.querySelectorAll('.curtain-link');
 
-  humburger.addEventListener('click', (event) => {
-    humburger.classList.toggle("open");
-    curtain.classList.toggle("open");
+
+
+
+  humburger.addEventListener('click', () => {
+    humburgerMenuAction()
   });
+
+  curtainLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+
+      humburgerMenuAction()
+
+      setTimeout(() => {
+        targetSection.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }, 1000);
+
+    })
+  })
+
+
+
+  
+  function humburgerMenuAction(){
+    humburger.classList.toggle('open');
+    curtain.classList.toggle('open');
+    document.body.classList.toggle('overflow')
+    line.classList.toggle('open');
+    sticker.classList.toggle('open');
+  }
+
+
+
